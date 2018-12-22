02c07e3b1a0b48a2117083ba3e1bbad46b55cbc0
Jujhar Singh <jujhar.pannu@gmail.com>
2018-12-22 02:02:46 -0800

    easy increment

--- counter.rb
+++ counter.rb
@@ -13,6 +13,30 @@ if input == 'y'
     count += 1
 end
 
+if input == 'yy'
+    count += 2
+end
+
+if input == 'yyy'
+    count += 3
+end
+
+if input == 'yyyy'
+    count += 4
+end
+
+if input == 'yyyyy'
+    count += 5
+end
+
+if input == 'yyyyyy'
+    count += 6
+end
+
+if input == 'yyyyyyy'
+    count += 7
+end
+
 if input == 'n'
     count -= 1
 end
